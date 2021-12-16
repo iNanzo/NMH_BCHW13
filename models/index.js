dcac482c5bfecdb40de0a19b1b7460c0314c9333
Newton Hoang <newtonhoang99@gmail.com>
2021-12-15 21:03:30 -0800

    index model update v1

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: "product_tag"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  through: "product_tag"
+});
 
 module.exports = {
   Product,
